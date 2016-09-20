ef93d1353ab28edd61fc74050925558b099ddf65
xaviermartinez2012 <xaviermartinez2012@gmail.com>
2016-09-20 15:10:52 -0700

    Added populate(...) function

--- Client0.cpp
+++ Client0.cpp
@@ -17,11 +17,12 @@
 
 using namespace std;
 
-void randString(char arr[]){
-    char cstring[101];
-    for(int i = 0; i < 100; i++){
-        srand(time(NULL));
-        cstring[i] = rand()%(90-65 + 1) + 65;
+void populate(char arr[][101], int size){
+    for (int i=0; i < size; i++) {
+        for (int j=0; j < 100; j++) {
+            srand(time(NULL));
+            arr[i][j] = rand()%(90-65 + 1) + 65;
+        }
     }
 }
 
@@ -35,10 +36,21 @@ int main(){
     };
     buf msg0;
     msg0.mtype = 1;
+    populate(msg0.arr, 20);
     
     buf msg1;
+    msg1.mtype = 2;
+    populate(msg1.arr, 20);
+    
     buf msg2;
+    msg2.mtype = 3;
+    populate(msg2.arr, 20);
+    
     int size = sizeof(buf)-sizeof(long);
+    
+    msgsnd(qid, (struct msgbuf *)&msg0, size, 0);
+    msgsnd(qid, (struct msgbuf *)&msg1, size, 0);
+    msgsnd(qid, (struct msgbuf *)&msg2, size, 0);
     
     return 0;
 }
