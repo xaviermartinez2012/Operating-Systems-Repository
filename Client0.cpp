6d6bfce6109a8b09ea10598871cd631047d935c0
Xavier Martinez <xavier@sorin11.campus.ad.csulb.edu>
2016-09-22 13:03:12 -0700

    Begginning of the end (edits)

--- Client0.cpp
+++ Client0.cpp
@@ -15,33 +15,34 @@
 #include <unistd.h>
 #include <cstdlib>
 #include <string>
+#include <ctime>
 
 using namespace std;
 
-void populate(char *arr, char client, int index){
-    int ones = index % 10;
-    int tens = index / 10;
-    srand(time(NULL));
-    for (int i = 0; i < 104; i++) {
-        if (i == 0) {
-            arr[i] = '0';
-            continue;
-        }
-        if (i == 1) {
-            arr[i] = client;
-            continue;
-        }
-        if (i == 2) {
-            arr[i] = (char)('0' + tens);
-            continue;
-        }
-        if (i == 3) {
-            arr[i] = (char)('0' + ones);
-            continue;
-        }
-        arr[i] = rand()%(90-65 + 1) + 65;
-    }
-}
+// void populate(char *arr, char client, int index){
+//     int ones = index % 10;
+//     int tens = index / 10;
+//     srand(time(NULL));
+//     for (int i = 0; i < 104; i++) {
+//         if (i == 0) {
+//             arr[i] = '0';
+//             continue;
+//         }
+//         if (i == 1) {
+//             arr[i] = client;
+//             continue;
+//         }
+//         if (i == 2) {
+//             arr[i] = (char)('0' + tens);
+//             continue;
+//         }
+//         if (i == 3) {
+//             arr[i] = (char)('0' + ones);
+//             continue;
+//         }
+//         arr[i] = rand()%(90-65 + 1) + 65;
+//     }
+// }
 
 struct mBuf {
     long mtype; // required
@@ -54,17 +55,16 @@ struct rBuf {
 };
 
 int main(){
+    srand(time(0));
     int qid = msgget(ftok(".",'u'), 0);
-    
     string requestHeader = "11";
     char client = '1';
     
     for (int i = 0; i < 20; i ++) {
-        srand(time(NULL));
         mBuf message;
         message.mtype = 111;
         int messageSize = sizeof(message)-sizeof(long);
-        string messageHeader = "01";
+        string messageHeader = "0" + client;
         int ones = i % 10;
         int tens = i / 10;
         messageHeader = messageHeader + (char)('0' + tens);
@@ -89,13 +89,14 @@ int main(){
         mBuf message;
         message.mtype = 111;
         int messageSize = sizeof(message)-sizeof(long);
-        message.message[0] = '1';
-        message.message[1] = client;
+        string messageHeader = "1" + client;
         cout << "Enter index: ";
         cin >> userChoice;
         if (userChoice != -1) {
-            message.message[2] = '2';
-            message.message[3] = '0';
+            int ones = userChoice % 10;
+            int tens = userChoice / 10;
+            messageHeader = messageHeader + (char)('0' + tens);
+            messageHeader = messageHeader + (char)('0' + ones);
             msgsnd(qid, (struct msgbuf *)&message, messageSize, 0);
         }
         else {
