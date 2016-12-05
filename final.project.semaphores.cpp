805254b7974436eca68af682e01ae73f83f52d98
xaviermartinez2012 <xaviermartinez2012@gmail.com>
2016-12-05 12:43:43 -0800

    Final Project draft 1

--- final.project.semaphores.cpp
+++ final.project.semaphores.cpp
@@ -26,10 +26,27 @@ void husband_process(SEMAPHORE &, double *, int);
 void wife_process(SEMAPHORE &, double *);
 void account_status(double *);
 
+void parentCleanup(SEMAPHORE &sem, int shmid) {
+    shmctl(shmid, IPC_RMID, NULL);
+    sem.remove();
+}
+
 int main(){
     srand(time(NULL));
+    cout.precision(2);
     
-    double accounts[] = {500000, 10000, 5000, 1000000, 10000, 30000, 5000, 300000};
+    int shm_id;
+    shm_id = shmget(IPC_PRIVATE, 8 * sizeof(double), PERMS);
+    
+    double * accounts = (double *) shmat(shm_id, NULL, 0);
+    accounts[0] = 500000;
+    accounts[1] = 10000;
+    accounts[2] = 5000;
+    accounts[3] = 1000000;
+    accounts[4] = 10000;
+    accounts[5] = 30000;
+    accounts[6] = 5000;
+    accounts[7] = 300000;
     
     SEMAPHORE sem(8);
     sem.V(SAVINGS);
@@ -53,50 +70,42 @@ int main(){
     for (int i = 0; i < 2; i++){
         wait(&status);
     }
-    parent_cleanup(sem);
+    parentCleanup(sem, shm_id);
     
     exit(0);
 }
 
-void parent_cleanup (SEMAPHORE &sem) {
-    sem.remove();
-}
 
 void husband_process(SEMAPHORE & sem, double * accounts, int process_identifier){
-    bool tf [] = {true, false};
     int randnum = rand();
     int transactions = 0;
     while (transactions < 100) {
-        if (tf[(randnum % 2)]) {
-	    cout << "Process " << process_identifier << " in here." << endl;
+        if (randnum % 37003) {
             int randnum2 = (rand() % 3) + 1;
             switch (randnum2) {
                 
                 case 1:
                     sem.P(SAVINGS);
-                    cout << "Process ID = " << process_identifier << ". Operation 1: Locked accounts." << endl;
-                    account_status(accounts);
-                    accounts[SAVINGS] += (accounts[SAVINGS] / 100.00);
-                    account_status(accounts);
-                    cout << "Process ID = " << process_identifier << ". Unlocking accounts." << endl;
+                    cout << "Process ID = " << process_identifier << ". Operation 1: Locked Savings." << endl;
+                    *(accounts + SAVINGS) += (accounts[SAVINGS] / 100.00);
+                    cout << fixed << "Savings: $" << accounts[SAVINGS] << endl;
+                    cout << "Process ID = " << process_identifier << ". Unlocking Savings." << endl;
                     sem.V(SAVINGS);
                     break;
                 case 2:
                     sem.P(IRA);
-                    cout << "Process ID = " << process_identifier << ". Operation 2: Locked accounts." << endl;
-                    account_status(accounts);
-                    accounts[IRA] += (accounts[IRA] * (3.00/100.00));
-                    account_status(accounts);
-                    cout << "Process ID = " << process_identifier << ". Unlocking accounts." << endl;
+                    cout << "Process ID = " << process_identifier << ". Operation 2: Locked IRA." << endl;
+                    *(accounts + IRA) += (accounts[IRA] * (3.00/100.00));
+                    cout << fixed << "IRA: $" << accounts[IRA] << endl;
+                    cout << "Process ID = " << process_identifier << ". Unlocking IRA." << endl;
                     sem.V(IRA);
                     break;
                 case 3:
                     sem.P(CHECKING);
-                    cout << "Process ID = " << process_identifier << ". Operation 3: Locked accounts." << endl;
-                    account_status(accounts);
-                    accounts[CHECKING] += 1500.00;
-                    account_status(accounts);
-                    cout << "Process ID = " << process_identifier << ". Unlocking accounts." << endl;
+                    cout << "Process ID = " << process_identifier << ". Operation 3: Locked Checking." << endl;
+                    *(accounts + CHECKING) += 1500.00;
+                    cout << fixed << "Checking: $" << accounts[CHECKING] << endl;
+                    cout << "Process ID = " << process_identifier << ". Unlocking Checking." << endl;
                     sem.V(CHECKING);
                     break;
 
@@ -105,16 +114,17 @@ void husband_process(SEMAPHORE & sem, double * accounts, int process_identifier)
             }
             transactions++;
         }
+        randnum = rand();
     }
 }
 
 void account_status(double * accounts){
-    cout << "Savings: $" << accounts[SAVINGS] << endl;
-    cout << "Checking: $" << accounts[CHECKING] << endl;
-    cout << "Money Market: $" << accounts[MONEY_MARKET] <<endl;
-    cout << "IRA: $" << accounts[IRA] <<endl;
-    cout << "Credit Card: $" << accounts[CARD] <<endl;
-    cout << "Auto Loan: $" << accounts[AUTO] <<endl;
-    cout << "Line of Credit: $" << accounts[LOC] << endl;
-    cout << "Mortgage: $" << accounts[MORTGAGE] <<endl;
+    cout << fixed << "Savings: $" << accounts[SAVINGS] << endl;
+    cout << fixed <<"Checking: $" << accounts[CHECKING] << endl;
+    cout << fixed << "Money Market: $" << accounts[MONEY_MARKET] <<endl;
+    cout << fixed << "IRA: $" << accounts[IRA] <<endl;
+    cout << fixed << "Credit Card: $" << accounts[CARD] <<endl;
+    cout << fixed << "Auto Loan: $" << accounts[AUTO] <<endl;
+    cout << fixed << "Line of Credit: $" << accounts[LOC] << endl;
+    cout << fixed << "Mortgage: $" << accounts[MORTGAGE] <<endl;
 }
